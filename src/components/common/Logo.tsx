a80e3f6f4754053e85ffc63e6cfe4ca84ab7a877
nedfusion <32440074+nedfusion@users.noreply.github.com>
2026-02-03 10:46:22 +0100

    Updated index.html

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -9,7 +9,7 @@ const Logo: React.FC<LogoProps> = ({ className = '' }) => {
   return (
     <div className={`flex items-center ${className}`}>
       <img
-        src="/nerd-logo.png"
+        src="/nerd.jpg"
         alt="Nerd Logistics - Professional Logistics and Transportation Services"
         className="h-20 w-auto object-contain"
       />
